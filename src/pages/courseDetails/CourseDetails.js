49114dce5f0c6713c4ff37ae71f69719d9405689
Kerolos <kiroloss.ashraff@gmail.com>
2022-02-20 15:01:33 +0200

    add new features

--- src/pages/courseDetails/CourseDetails.js
+++ src/pages/courseDetails/CourseDetails.js
@@ -25,10 +25,10 @@ export default function CourseDetails() {
                 interpreted, object-oriented language with first-class
                 functions,
               </p>
-              <div className="">
-                <div className="">
-                  <div className="col-md-12 px-0">
-                    <div className="stars">
+              <div className="h-2-5rem">
+                <div className="h-100">
+                  <div className="col-md-12 px-0 h-100">
+                    <div className="stars h-100">
                       <form action="">
                         {" "}
                         <input
@@ -37,35 +37,35 @@ export default function CourseDetails() {
                           type="radio"
                           name="star"
                         />{" "}
-                        <label className="star star-5" for="star-5"></label>{" "}
+                        <label className="star star-5" htmlFor="star-5"></label>{" "}
                         <input
                           className="star star-4"
                           id="star-4"
                           type="radio"
                           name="star"
                         />{" "}
-                        <label className="star star-4" for="star-4"></label>{" "}
+                        <label className="star star-4" htmlFor="star-4"></label>{" "}
                         <input
                           className="star star-3"
                           id="star-3"
                           type="radio"
                           name="star"
                         />{" "}
-                        <label className="star star-3" for="star-3"></label>{" "}
+                        <label className="star star-3" htmlFor="star-3"></label>{" "}
                         <input
                           className="star star-2"
                           id="star-2"
                           type="radio"
                           name="star"
                         />{" "}
-                        <label className="star star-2" for="star-2"></label>{" "}
+                        <label className="star star-2" htmlFor="star-2"></label>{" "}
                         <input
                           className="star star-1"
                           id="star-1"
                           type="radio"
                           name="star"
                         />{" "}
-                        <label className="star star-1" for="star-1"></label>{" "}
+                        <label className="star star-1" htmlFor="star-1"></label>{" "}
                       </form>
                     </div>
                   </div>
